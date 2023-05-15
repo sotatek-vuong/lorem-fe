d8c6dedab7fc469df368a215b04e8801c429458c
vuong-sotatek <hoang.vuong@sotatek.com>
2023-05-15 21:59:08 +0700

    first commit

---
+++ src/components/header.svelte
@@ -0,0 +1,9 @@
+<nav class="border-b border-surface-500">
+	<div class="md:container md:mx-auto px-4 py-2 flex items-center gap-2">
+		<a href="/" class="h2 text-primary-500 text">Logo</a>
+		<div class="flex-1" />
+
+		<button type="button" class="btn hover:variant-soft-surface">Login</button>
+		<button type="button" class="btn variant-filled-primary">Register</button>
+	</div>
+</nav>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script>
+	// Your selected Skeleton theme:
+	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+
+	// This contains the bulk of Skeletons required styles:
+	// NOTE: this will be renamed skeleton.css in the v2.x release.
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
+	import '../app.postcss';
+
+	import { AppShell } from '@skeletonlabs/skeleton';
+	import Header from '../components/header.svelte';
+</script>
+
+<AppShell>
+	<svelte:fragment slot="header"><Header /></svelte:fragment>
+
+	<slot />
+	<svelte:fragment slot="pageFooter">
+		<hr />
+		Page Footer
+	</svelte:fragment>
+</AppShell>
